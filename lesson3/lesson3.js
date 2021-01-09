2ab47be66f147974531c2a79870ba7d7e05054f5
mrwindmark <agentstorm@ya.ru>
2021-01-09 16:13:41 +0300

    Complite Task 4 and 5

--- lesson3/lesson3.js
+++ lesson3/lesson3.js
@@ -10,3 +10,13 @@ while(i < 100) {
         i++;
     }
 }
+
+for (i = 0; i < 10; i++) alert(i+1);
+
+i = 0;
+resultString = "*";
+do {
+    console.log(resultString);
+    resultString += "*";
+    i++;
+}while (i < 20);
